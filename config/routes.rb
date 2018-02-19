16c2c84cc7fd03a585490872f9d3656925b7a134
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-02-19 02:05:24 -0500

    posts scaffold charges controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :charges, only: [:new, :create]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: "page#home"
+  root to: "posts#index"
 end
