94a2aa1da414dedd2234f6bb701d58015beb9e3e
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-02-19 09:24:07 -0500

    subscription

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,22 +2,29 @@ class ChargesController < ApplicationController
   
   before_action :amount_to_be_charged
   before_action :set_description
+  before_action :set_plan
   before_action :authenticate_user!
 
   def new
   end
 
   def create
-  	customer = StripeTool.create_customer(
-  		email: params[:stripeEmail],
-  		stripe_token: params[:stripeToken]
-  	)
+  	if params[:subscription].include? 'yes'
+  		StripeTool.create_membership(
+  			email: params[:stripeEmail],
+  			stripe_token: params[:stripeToken],
+  			plan: @plan
+  		)
+  	else
+	  	customer = StripeTool.create_customer(
+	  		email: params[:stripeEmail],
+	  		stripe_token: params[:stripeToken])
 
-  	charge = StripeTool.create_charge(
-  		customer_id: customer.id,
-  		amount: @amount,
-  		description: @description
-  	)
+	  	charge = StripeTool.create_charge(
+	  		customer_id: customer.id,
+	  		amount: @amount,
+	  		description: @description)
+  	end
   	
   	redirect_to thanks_path
 	  rescue Stripe::CardError => e
@@ -37,6 +44,10 @@ class ChargesController < ApplicationController
 
   	def set_description
   		@description = "Zen Liquidator"
+  	end
+
+  	def set_plan
+  		@plan = 9999
   	end
 
 end
